95716ff989c2d9d8980180bf59663e1e1739ee43
Jose Oliva <its.joseoliva@gmail.com>
2024-05-23 13:00:31 -0600

    prueba videowall 11

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -49,7 +49,7 @@ export const Experience = () => {
     if (cameraZoomed) {
       cameraControls.current.setLookAt(-0.3, 1.5, 1.5, 0.45, 1.5, 0, true);
     } else {
-      cameraControls.current.setLookAt(1.4, 1.4, 1.9, -0.5, 1.9, 1.4, true);
+      cameraControls.current.setLookAt(1.4, 1.4, 1.9, -0.5, 1.9, 1.5, true);
     }
   }, [cameraZoomed]);
   return (
