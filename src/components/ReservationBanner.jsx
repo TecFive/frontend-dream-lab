17ce5981f11ceccf5e5663ca4511d854b3592961
MarioFdz <a00834434@tec.mx>
2024-05-24 14:25:44 -0600

    success get reservations

--- src/components/ReservationBanner.jsx
+++ src/components/ReservationBanner.jsx
@@ -12,7 +12,7 @@ const ReservationBanner = () => {
   const [indexReservation, setIndexReservation] = useState(0);
 
   useEffect(() => {
-    axios.get("https://dream-lab-backend.azurewebsites.net/v1/reservations", {
+    axios.get("/v1/reservations/", {
       headers: {
         Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2MWQ3NmNkZmM2Y2RhNmQ1MGNmMDQ3MiIsIm5hbWUiOiJQQVRSSUNJTyBWSUxMQVJSRUFMIiwiZW1haWwiOiJBMDA4MzQ1MjYiLCJjYXJlZXIiOiJJVEMiLCJzZW1lc3RlciI6Niwicm9sZSI6IlN0dWRlbnQifQ.JCPU9TCV6JEpushJ1VllSlMBMzFTPJY4dNuL0Em6SgI"
       }
